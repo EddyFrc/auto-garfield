7760db6c1eb046dae3144462eb62efdcec1fd5f0
EDDY FRANCOU <efrancou@etu.uqac.ca>
2025-09-08 20:45:27 -0400

    Add main classes

---
+++ Source/AutoGarfield/Public/Jon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "Jon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AUTOGARFIELD_API AJon : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
