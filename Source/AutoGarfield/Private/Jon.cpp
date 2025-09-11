51a6a9524bfa167a0311fc5a9474148fbdafa6a8
EDDY FRANCOU <efrancou@etu.uqac.ca>
2025-09-11 18:43:48 -0400

    Essai pour implémenté l'IA de Jon

--- Source/AutoGarfield/Private/Jon.cpp
+++ Source/AutoGarfield/Private/Jon.cpp
@@ -9,21 +9,37 @@ void AJon::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 	// Call Seek
+	CurrentForce = Seek(CurrentTarget);
 	// Apply force using the resulting vector
-	// Check if colliding with point or near it
-	// If colliding, call OnControlPointReached
+	LaunchCharacter(CurrentForce, true, true);
+	// Check if near the defined target
+	if ((this->GetActorLocation() - CurrentTarget).Y < 5)
+	{
+		// If near the target, call OnControlPointReached
+		OnControlPointReached();
+	}
 }
 
 void AJon::OnControlPointReached()
 {
 	// Generate new lasagna for Garfield to eat
-	GetWorld()->SpawnActor<ALasagna>(ALasagna::StaticClass(), this->GetActorLocation(), this->GetActorRotation());
-	// Delete old point
+	GetWorld()->SpawnActor<ALasagna>(LasagnaToSpawn, this->GetActorLocation(), this->GetActorRotation());
 	// Create new random point
+	CurrentTarget = FVector(this->GetActorLocation().X, FMath::RandRange(-550.0, 550.0), this->GetActorLocation().Z);
 }
 
 
 void AJon::BeginPlay()
 {
 	OnControlPointReached();
+}
+
+FVector AJon::Seek(const FVector& Target)
+{
+	FVector Desired = Target - this->GetActorLocation();
+	Desired.Normalize();
+	Desired *= 100.0;
+	FVector Steering = Desired - this->GetVelocity();
+	Steering = Steering.GetClampedToMaxSize(100.0);
+	return Steering;
 }
--- Source/AutoGarfield/Public/Jon.h
+++ Source/AutoGarfield/Public/Jon.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Lasagna.h"
 #include "PaperCharacter.h"
+#include "src/Core/Matrix.h"
 #include "Jon.generated.h"
 
 /**
@@ -13,11 +15,19 @@ UCLASS()
 class AUTOGARFIELD_API AJon : public APaperCharacter
 {
 	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Spawning")
+    TSubclassOf<ALasagna> LasagnaToSpawn;
+
+	FVector CurrentTarget;
+	FVector CurrentForce;
 
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaSeconds) override;
 
 	void OnControlPointReached();
+
+	FVector Seek(const FVector& Target);
 	
 };
